b1436d31dfc22d2bc4b9301ee98ddbb7b5f491b0
ranggatw <rangga.public@gmail.com>
2022-02-10 00:37:26 +0700

    no match routing

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import About from "./component/About";
 import { Home } from "./component/Home";
 import Navbar from "./component/Navbar";
+import { NoMatch } from "./component/NoMatch";
 import { OrderSummary } from "./component/OrderSummary";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />}></Route>
         <Route path="about" element={<About />}></Route>
         <Route path="order-summary" element={<OrderSummary />}></Route>
+        <Route path="*" element={<NoMatch />}></Route>
       </Routes>
     </>
   );
