8fde2a28c914ca21446b60ff49d756ab9eb32009
ranggatw <rangga.public@gmail.com>
2022-02-09 22:04:30 +0700

    diffrent the Link and Navlink

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,12 +1,33 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 function Navbar() {
+  const navLinkStyles = ({ isActive }) => {
+    // destructured in function parameter isActive
+    // isActive default will be set "true" if the link is indeed the current route
+    return {
+      fontWeight: isActive ? "bold" : "normal",
+      textDecoration: isActive ? "none" : "underline",
+    };
+  };
+
   return (
     <nav>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
+      {/* 1. Navlink have a active class for styling */}
+      {/* <NavLink to="/about">About</NavLink> */}
+      {/* 4. We can create style prop in this navlinks */}
+      <NavLink style={navLinkStyles} to="/">
+        Home
+      </NavLink>
+      <NavLink style={navLinkStyles} to="/about">
+        About
+      </NavLink>
     </nav>
   );
 }
 
 export default Navbar;
+
+
+
+// Notes :
+// Navlink is specific use for create component like : Navbar, Breadcrumb or a set of tabs. Where you like to highlight the current selected items 
