691de50fab05789d09316c17c24372dacdeec1f0
ranggatw <rangga.public@gmail.com>
2022-02-09 21:47:16 +0700

    Links

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import { Routes, Route } from "react-router-dom";
 import About from "./component/About";
 import Home from "./component/Home";
+import Navbar from "./component/Navbar";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />}></Route>
-      <Route path="about" element={<About />}></Route>
-    </Routes>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="about" element={<About />}></Route>
+      </Routes>
+    </>
   );
 }
 
